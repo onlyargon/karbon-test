b56d16cb9dfcc0060ed2b9db1993099fcf1543b9
onlyargon <arunawickram@gmail.com>
2026-02-05 09:19:27 +0530

    Complete openf1client

--- F1DriversApi/External/OpenF1Client.cs
+++ F1DriversApi/External/OpenF1Client.cs
@@ -1,21 +1,75 @@
+using System.Text.Json;
 using F1DriversApi.Models;
 
 namespace F1DriversApi.External;
 
-public class OpenF1Client: IOpenF1Client
+public class OpenF1Client(HttpClient client) : IOpenF1Client
 {
-    public Task<IEnumerable<OpenF1DriverResponse>> GetDriversAsync(string sessionKey = "latest")
+    private readonly JsonSerializerOptions _jsonOptions = new()
     {
-        throw new NotImplementedException();
+        PropertyNameCaseInsensitive = true
+    };
+
+    public async Task<IEnumerable<OpenF1DriverResponse>> GetDriversAsync(string sessionKey = "latest")
+    {
+        var url = $"drivers/session_key={sessionKey}";
+
+        try
+        {
+            var res = await client.GetAsync(url);
+            res.EnsureSuccessStatusCode();
+            
+            var drivers = await res.Content.ReadFromJsonAsync<List<OpenF1DriverResponse>>(_jsonOptions);
+
+            if (drivers is null || drivers.Count == 0)
+            {
+                return Array.Empty<OpenF1DriverResponse>();
+            }
+
+            return drivers;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 
-    public Task<OpenF1DriverResponse?> GetDriverByNumberAsync(int driverNumber, string sessionKey = "latest")
+    public async Task<OpenF1DriverResponse?> GetDriverByNumberAsync(int driverNumber, string sessionKey = "latest")
     {
-        throw new NotImplementedException();
+        var url = $"drivers/driver_number={driverNumber}&session_key={sessionKey}";
+
+        try
+        {
+            var res = await client.GetAsync(url);
+            res.EnsureSuccessStatusCode();
+            
+            var driver = await res.Content.ReadFromJsonAsync<List<OpenF1DriverResponse>>(_jsonOptions);
+
+            if (driver is null || driver.Count == 0)
+            {
+                return null;
+            }
+            
+            return driver.FirstOrDefault();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 
-    public Task<int> GetLatestSessionKeyAsync()
+    public async Task<int> GetLatestSessionKeyAsync()
     {
-        throw new NotImplementedException();
+        var drivers = await GetDriversAsync();
+        var firstDriver = drivers.FirstOrDefault();
+
+        if (firstDriver is null)
+        {
+            return 0;
+        }
+        
+        return firstDriver.SessionKey;
     }
 }
--- F1DriversApi/Models/OpenF1Config.cs
+++ F1DriversApi/Models/OpenF1Config.cs
@@ -2,6 +2,6 @@ namespace F1DriversApi.Models;
 
 public class OpenF1Config
 {
-    public string BaseUrl { get; set; } = "https://api.openf1.org/v1/drivers";
+    public string BaseUrl { get; set; } = "https://api.openf1.org/v1/";
     public const string SectionName = "OpenF1";
 }
